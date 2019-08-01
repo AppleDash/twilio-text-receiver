95c098deeb24ab30e78da168d68b7508864e9c2b
AppleDash <pet@feather.horse>
2019-08-01 03:55:33 -0400

    Add SQL stuffs

--- app.py
+++ app.py
@@ -1,3 +1,24 @@
+import os
 import flask
+import flask_migrate
+
+import flask_sqlalchemy as sqlalchemy
 
 app = flask.Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
+db = sqlalchemy.SQLAlchemy()
+migrate = flask_migrate.Migrate(app, db)
+
+# this is down here because message.py imports db from app.py (this file), so it needs to be initialized first.
+from models.message import Message
+
+# this function handles the webhook endpoint, which is called by Twilio when we receive a text on our number.
+@app.route('/webhook')
+def handle_webhook():
+    pass
+
+
+# this function handles the root endpoint, which is where texts that have been received will be displayed.
+@app.route('/')
+def handle_index():
+    pass
---
+++ models/message.py
@@ -0,0 +1,17 @@
+from app import db
+from sqlalchemy.dialects.postgresql import UUID
+
+
+class Message(db.Model):
+    __tablename__ = 'messages'
+    id = db.Column(UUID, primary_key=True)
+    received_at = db.Column(db.DateTime, nullable=False)
+    from_number = db.Column(db.Text, nullable=False)
+    to_number = db.Column(db.Text, nullable=False)
+    body = db.Column(db.Text, nullable=False)
+
+    def __init__(self, received_at, from_number, to_number, body):
+        self.received_at = received_at
+        self.from_number = from_number
+        self.to_number = to_number
+        self.body = body
